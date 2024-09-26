4c4b43edfe0881be64f2a2bba842c6ef2ced111d
Shehbaaz11 <162619054+Shehbaaz11@users.noreply.github.com>
2024-09-26 20:08:42 +0530

    Add files via upload

---
+++ ChatBot.py
@@ -0,0 +1,27 @@
+# import time
+# import random
+# now =time.ctime()
+# print("Hi! i am your BOT_FRIEND :)\n")
+# name = input("What is your name?")
+
+
+# time.sleep(1)
+# print("HELLO " +name+",")
+
+# feeling =input("how are you today?")
+# time.sleep(1)
+# if "good" in feeling and "not" not in feeling:
+#     print("Im feeling good too")
+# else :
+#     print ("Im sorry to hear that !")
+
+# time.sleep(1)
+# favcolour=input("what is your favourite colour?")
+# time.sleep(1)
+# colours =["red","blue","orange","black"]
+# print("Ohh Great! Bytheway My favourite colour is "+random.choice(colours))
+
+# question=input()
+# if "time" in question:
+#     print(now)
+  
